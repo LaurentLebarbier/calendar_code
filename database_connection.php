c8851e9c49d6a0e4a11c4bf3f76d3c6f717ec6dc
“Laurent <laurentlebarbier70@gmail.com>
2024-11-27 15:08:28 +0100

    Initial commit for calendar_code

---
+++ database_connection.php
@@ -0,0 +1,7 @@
+<?php
+$hostname = "localhost";
+$username = "laurent";
+$password = "h9xt2ya1";  
+$database = "calendar_event_master";   
+$con=mysqli_connect($hostname,$username,$password,$database);    
+?>   
